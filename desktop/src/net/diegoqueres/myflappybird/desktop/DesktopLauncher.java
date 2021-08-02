e0f322001ef49c5a2f04b57aa3804c143dff84b1
Diego Queres <diegoqueres81@gmail.com>
2021-08-02 19:50:33 -0300

    Commit inicial

---
+++ desktop/src/net/diegoqueres/myflappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.diegoqueres.myflappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.diegoqueres.myflappybird.MainClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainClass(), config);
+	}
+}
